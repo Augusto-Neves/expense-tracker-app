39685fd9b02e9ec4688deeebf9c7446218ef682c
Augusto Neves <augustoneves@frwk.com.br>
2023-06-15 23:12:24 -0300

    feat: configuring Manage Expense screen

--- screens/ManageExpenseScreen.js
+++ screens/ManageExpenseScreen.js
@@ -1,6 +1,16 @@
+import { useLayoutEffect } from "react";
 import { View, Text } from "react-native";
 
-export function ManageExpenseScreen() {
+export function ManageExpenseScreen({ route, navigation }) {
+  const editedExpenseId = route.params?.expenseId;
+  const isEditing = !!editedExpenseId;
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? "Editing Expense" : "Adding Expense",
+    });
+  }, [navigation, isEditing]);
+
   return (
     <View>
       <Text>ManegeExpenseScreens</Text>
